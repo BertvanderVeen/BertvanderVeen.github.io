970bd9bff2af551aa575f3eeec24db767bd3c0f3
BertvanderVeen <bert_van_der_veen@hotmail.com>
2026-03-06 14:01:02 +0000

    Deploying to gh-pages from @ BertvanderVeen/BertvanderVeen.github.io@80fba017c8134699fd9cec204e9fd8089a52f91e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,12 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
+  },{id: "nav-workshops",
+          title: "workshops",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/workshops/";
           },
         },{id: "nav-for-review-requests",
           title: "For review requests",
